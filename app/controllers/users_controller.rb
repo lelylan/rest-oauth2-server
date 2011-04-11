414f9ff89556c72e96da64f856706e0eb3d26d8b
Andrea Reginato <andrea.reginato@gmail.com>
2011-04-11 22:19:35 +0200

    user show action defined

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
 
   skip_before_filter :authenticate
 
+  def show
+    @user = User.criteria.id(params[:id]).first
+    resource_not_found unless @user
+  end
+
   def new
     @user = User.new
   end
@@ -15,5 +20,13 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+
+  private 
+
+    def resource_not_found
+      flash.now.alert = "notifications.document.not_found"
+      @info = { id: params[:id] }
+      render "shared/html/404" and return
+    end
 
 end
