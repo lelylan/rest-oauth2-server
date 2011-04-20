0aa56593a5427843e778020371f00cd31e684e73
Andrea Reginato <andrea.reginato@gmail.com>
2011-04-20 12:42:28 +0200

    Revert "Added redirect to last URI before logging in. Closes issue #9"
    This reverts commit ba1219cbb95e86886e650b05a6ccdd8d8e95b315.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to session[:return_to], :notice => "Logged in!"
-      #redirect_to clients_path, :notice => "Logged in!"
+      redirect_to clients_path, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
