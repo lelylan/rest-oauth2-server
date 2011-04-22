b6f7501a1b3cd7424b16e6b546cf5d6645fab442
Andrea Reginato <andrea.reginato@gmail.com>
2011-04-22 14:24:11 +0200

    Added a graph into the access detail

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-  //$('#tags').tagsInput();
-  //alert("hola");
-
 $(document).ready(function () {
   $('input.tags').tagsInput({
     'unique':true,
@@ -13,4 +10,43 @@ $(document).ready(function () {
     'width':'295px'
   });  
 });
+
+var chart;
+function createChart(days, times, uri) {
+   chart = new Highcharts.Chart({
+      chart: {
+         renderTo: 'chart',
+         defaultSeriesType: 'line',
+         marginRight: 30,
+         marginBottom: 50
+      },
+      title: { text: 'Daily access rate for client' },
+      subtitle: { text: uri },
+      xAxis: {
+         categories: days
+      },
+      yAxis: {
+         title: { text: 'Number of requests' },
+         plotLines: [{ value: 0, width: 1, color: '#808080' }]
+      },
+      tooltip: {
+         formatter: function() {
+           return 'Accesses ' + this.x +'<br/><b>' + this.y + ' times</b>';
+         }
+      },
+      legend: {
+         layout: 'vertical',
+         align: 'right',
+         verticalAlign: 'top',
+         x: -10,
+         y: 10,
+         borderWidth: 0
+      },
+      series: [{
+         name: 'accesses',
+         data: times
+      }]
+   });
+};
+
 
