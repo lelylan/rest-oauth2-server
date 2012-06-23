eef52e25da0d7d9b5e73708c12647f10ef46ab3c
unknown <Hajime@.(none)>
2012-06-24 00:02:51 +0900

    Uncaught exception: undefined method `env' for class `Capybara::Driver::RackTest'

--- config/initializers/capybara_headers_hack.rb
+++ config/initializers/capybara_headers_hack.rb
@@ -7,14 +7,14 @@ module RackTestMixin
         @hacked_env ||= {}
       end
       
-      # Alias the original method for further use.
-      alias_method  :original_env, :env
-
       # Override the method to merge additional headers.
       # Plus this implicitly makes it public.
       def env
         original_env.merge(hacked_env)
       end
+
+      # Alias the original method for further use.
+      alias_method  :original_env, :env
     end
   end
 
